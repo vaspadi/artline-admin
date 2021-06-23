f522019518d8813b6fd65b8ecd06c8d10f03e443
Павел Васильев <vasiliev.work1@gmail.com>
2021-06-23 15:27:15 +0300

    Add closing modal on click a close button

--- js/modal.js
+++ js/modal.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const modalOverlay = document.querySelector('.modal-overlay')
   const modal = document.querySelector('.modal')
   const button = document.querySelector('#show-modal')
+  const close = document.querySelector('#close-modal')
 
   button.addEventListener('click', function(e) {
     e.stopPropagation()
@@ -11,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
   window.addEventListener('click', function (e) {
     const contain = e.target === modal || modal.contains(e.target)
     
-    if (!contain) {
+    if (!contain || e.target === close) {
       modalOverlay.style.display = 'none'
     }
   })
