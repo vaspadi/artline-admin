6d7bd73ec47e6a3f9cf2f35beea579ffb9b96b71
Павел Васильев <vasiliev.work1@gmail.com>
2021-06-23 16:10:53 +0300

    Create modal on finance page

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
   const modalOverlay = document.querySelector('.modal-overlay')
   const modal = document.querySelector('.modal')
-  const button = document.querySelector('#show-modal')
+  const buttons = document.querySelectorAll('.show-modal')
   const close = document.querySelector('#close-modal')
 
-  button.addEventListener('click', function(e) {
-    e.stopPropagation()
-    modalOverlay.style.display = 'flex'
-  }) 
+  buttons.forEach(button => {
+    button.addEventListener('click', function(e) {
+      e.stopPropagation()
+      modalOverlay.style.display = 'flex'
+    }) 
+  })
 
   window.addEventListener('click', function (e) {
     const contain = e.target === modal || modal.contains(e.target)
