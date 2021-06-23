3f9a8d89419088f3c3b36503a805f27965782c16
Павел Васильев <vasiliev.work1@gmail.com>
2021-06-23 12:04:42 +0300

    Fix multiselct styles'

--- js/multiselect.js
+++ js/multiselect.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     option.className = 'multiselect__item'
     option.innerHTML = `<label class="multiselect__label">
                           <input class="radio__input" type="checkbox" value="${item.value}" name="sticker">
-                          <span class="radio__pseudo-radio"></span>
+                          <span class="multiselect__pseudo-radio radio__pseudo-radio"></span>
 
                           <div class="multiselect__img">
                             <img src="${item.img}" alt="">
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
   selectAll.className = 'multiselect__item'
   selectAll.innerHTML = `<label class="multiselect__label">
                             <input class="radio__input" type="checkbox" value="all" name="sticker">
-                            <span class="radio__pseudo-radio"></span>
+                            <span class="multiselect__pseudo-radio radio__pseudo-radio"></span>
 
                             <p class="multiselect__text">Всё</p>
                           </label>`
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function() {
   })
 
   // Создание селекта
-  const multiselectWrapper = document.querySelector('.multiselect-wrapper')
+  const multiselect = document.querySelector('.multiselect')
   const multiselectList = document.querySelector('.multiselect__list')
   const multiselectSearch = document.querySelector('.multiselect__search')
   let currentOptions = options
@@ -77,10 +77,12 @@ document.addEventListener('DOMContentLoaded', function() {
   })
 
   window.addEventListener('click', function(e) {
-    if (e.target === multiselectWrapper || multiselectWrapper.contains(e.target)) {
+    if (e.target === multiselect || multiselect.contains(e.target)) {
       multiselectList.style.display = 'flex'
+      multiselect.classList.remove("multiselect_no-active")
     } else {
       multiselectList.style.display = 'none'
+      multiselect.classList.add("multiselect_no-active")
       multiselectSearch.value = ''
 
       options.forEach(option => {
@@ -108,7 +110,7 @@ document.addEventListener('DOMContentLoaded', function() {
     options.forEach(option => {
       const name = option.querySelector('.multiselect__text').textContent.toLowerCase()
 
-      if (name.indexOf(value) !== -1) {
+      if (name.indexOf(value.toLowerCase()) !== -1) {
         option.style.display = 'block'
         
       } else {
