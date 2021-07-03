ebc4232b0c7fcc425acb59dd55b82cefbb7befa3
Павел Васильев <vasiliev.work1@gmail.com>
2021-07-03 17:14:13 +0300

    Create RadioInput class

--- js/information.js
+++ js/information.js
@@ -1,26 +1,38 @@
-window.addEventListener('DOMContentLoaded', function() {
-  const price = document.querySelectorAll('input[name="price"]')
-  const payId = document.getElementById('pay-id')
+// window.addEventListener('DOMContentLoaded', function() {
 
-  price.forEach(item => {
-    item.addEventListener('change', function() {
-      if(this.value === "pay") {
-        payId.disabled = false
-      } else {
-        payId.disabled = true
-      }
+//   const bannerCheckbox = document.getElementById('banner-checkbox')
+//   const bannerButton = document.getElementById('banner-button')
+
+//   bannerCheckbox.addEventListener('change', function() {
+//     if (this.checked) {
+//       bannerButton.disabled = false
+//     } else {
+//       bannerButton.disabled = true
+//     }
+//   })
+// })
+
+class RadioInput {
+  constructor(params) {
+    this.radioButtons = document.querySelectorAll(`input[name="${params.radioName}"]`)
+    this.input = document.getElementById(params.inputId)
+    this._isDisabled = false
+  }
+
+  get isDisabled() {
+    return this._isDisabled
+  }
+
+  set isDisabled(value) {
+    this.input.disabled = value
+    this._isDisabled = value
+  }
+
+  init() {
+    this.radioButtons.forEach(radio => {
+      radio.addEventListener('change', () => {
+        this.isDisabled = radio.value === 'disabled'
+      })
     })
-  })
-
-
-  const bannerCheckbox = document.getElementById('banner-checkbox')
-  const bannerButton = document.getElementById('banner-button')
-
-  bannerCheckbox.addEventListener('change', function() {
-    if (this.checked) {
-      bannerButton.disabled = false
-    } else {
-      bannerButton.disabled = true
-    }
-  })
-})
+  }
+}
