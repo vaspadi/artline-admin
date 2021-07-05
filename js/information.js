e9ff43d45b9dbf70a2a4fd0fe1f41c822e6f27d2
Павел Васильев <vasiliev.work1@gmail.com>
2021-07-05 18:15:59 +0300

    Change the state of the second invoice according to the first

--- js/information.js
+++ js/information.js
@@ -14,24 +14,51 @@
 
 class RadioInput {
   constructor(params) {
-    this.radioButtons = document.querySelectorAll(`input[name="${params.radioName}"]`)
-    this.input = document.getElementById(params.inputId)
-    this._isDisabled = false
+    this.active = params.active
+    this.radio = document.getElementById(params.id)
+    this.labels = this.radio.querySelectorAll('.radio')
+    this.radioButtons = this.radio.querySelectorAll(`input[type="radio"]`)
+    this.activeRadio = this.radio.querySelector(`input[value="not-disabled"]`)
+    this.input = this.radio.querySelector('input[type="text"]')
+    this._inputDisabled = false
   }
 
-  get isDisabled() {
-    return this._isDisabled
+  get inputDisabled() {
+    return this._inputDisabled
   }
 
-  set isDisabled(value) {
+  set inputDisabled(value) {
     this.input.disabled = value
-    this._isDisabled = value
+    this._inputDisabled = value
   }
 
+  setDisabled() {
+    this.labels.forEach(label => {
+      label.classList.add('radio_disabled')
+    })
+    this.radioButtons.forEach(radio => {
+      radio.disabled = true
+    })
+    this.inputDisabled = true
+  }
+
+  setActive() {
+    this.labels.forEach(label => {
+      label.classList.remove('radio_disabled')
+    })
+    this.radioButtons.forEach(radio => {
+      radio.disabled = false
+
+      if (radio.checked && radio.value === 'not-disabled') this.inputDisabled = false
+    })
+  }
+  
   init() {
+    if (this.active !== undefined && !this.active) this.setDisabled()
+
     this.radioButtons.forEach(radio => {
       radio.addEventListener('change', () => {
-        this.isDisabled = radio.value === 'disabled'
+        this.inputDisabled = radio.value === 'disabled'
       })
     })
   }
